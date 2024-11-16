dc5f2907035cf442d23806a1a6a71a49468d2624
Aneesh165 <aneeshmp165@gmail.com>
2024-11-16 13:07:59 +0530

    16-11-24

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Gallery from "./Componenets/Gallery";
 import Promotions from "./Componenets/Promotions";
 import Blog from "./Componenets/Blog";
 import Faq from "./Componenets/Faq";
+import BookingCrad from "./Componenets/BookingCrad";
 
 const App = () => {
   return <div>
@@ -18,6 +19,7 @@ const App = () => {
     <Promotions/>
     <Blog/>
     <Faq/>
+    <BookingCrad/>
   </div>;
 };
 
